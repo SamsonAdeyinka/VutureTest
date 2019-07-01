59a883da0ecc79d7c06a2d7f0888b941e38401c2
SamsonAdeyinka <samsonadeyinka@hotmail.co.uk>
2019-07-01 14:44:24 +0100

    Task3A done

--- Test/src/Task3A.java
+++ Test/src/Task3A.java
@@ -1,30 +1,59 @@
-import java.awt.List;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Task3A {
 
-	static int occur(String str, String word) {
-		
-		int occ = 0;
-		String[] x = str.split(" ");
-		
-		for (int i = 0; i < x.length; i++) {
-			
-			if (word.equals(x[i])) {
-				occ++;
+	static int occur(String str, String censor) {
+		String a[] = str.split(" ");
+		String b[] = censor.split(", ");
+
+		int count = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (censor.equals(a[i]))
+				count++;
+		}
+		return count;
+	}
+
+	static void counter(String input, String censor) {
+		HashMap<String, Integer> strCount = new HashMap<String, Integer>();
+		String[] strArray = input.split(" ");
+		String[] strList = censor.split(", ");
+
+		for (String str : strArray) {
+			for (int i = 0; i < strList.length; i++) {
+				if (strList[i].equals(str)) {
+					if (strCount.containsKey(str)) {
+						strCount.put(str, strCount.get(str) + 1);
+					} else {
+						strCount.put(str, 1);
+					}
+				}
 			}
 		}
-		return occ;
-	}
-	
-	
+		for (Map.Entry entry : strCount.entrySet()) {
+			System.out.println(entry.getKey() + " occurs: " + entry.getValue() + " times");
+		}
+
+	} 
+
 	public static void main(String[] args) {
-
-		String str = "Hello my name is Samson name";
-		String word = "name";
-		System.out.println(occur(str, word));
 		
-	}
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter sentence or word: ");
+		String str = scan.nextLine().toLowerCase();
 
+		System.out.println("Enter words you'll like to censor (use commers): ");
+		String censor = scan.nextLine().toLowerCase();
+		
+		counter(str, censor);
+		
+		scan.close();
+		
+		
+
+
+	}
 }
+
